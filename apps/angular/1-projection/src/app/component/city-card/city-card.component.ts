65892be3d2c5fd5e5511ba2dddf753df25d8a2d3
Pablo Pastorino <phpastorino@outlook.com>
2024-08-06 11:29:50 -0300

    feat: city card

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      [list]="cities"
+      itemName="name"
+      [store]="store"
+      [randomData]="randCity"
+      imgSrc="assets/img/city.png"
+      [backgroundColor]="backgroundColor"></app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
+  randCity: () => City = randomCity;
+  backgroundColor = 'rgba(0, 0, 250, 0.1)';
 
-  ngOnInit(): void {}
+  constructor(
+    private http: FakeHttpService,
+    public store: CityStore,
+  ) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((t) => this.store.addAll(t));
+    this.store.cities$.subscribe((t) => (this.cities = t));
+  }
 }
