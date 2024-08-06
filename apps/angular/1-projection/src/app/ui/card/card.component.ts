11bc3c5267726fb8313d807abbf43574597f6a86
Pablo Pastorino <phpastorino@outlook.com>
2024-08-06 11:26:39 -0300

    refactor: card component to make it a dummy component

--- apps/angular/1-projection/src/app/ui/card/card.component.ts
+++ apps/angular/1-projection/src/app/ui/card/card.component.ts
@@ -1,9 +1,6 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
+import { Store } from '../../model/store.model';
 import { ListItemComponent } from '../list-item/list-item.component';
 
 @Component({
@@ -11,24 +8,15 @@ import { ListItemComponent } from '../list-item/list-item.component';
   template: `
     <div
       class="flex w-fit flex-col gap-3 rounded-md border-2 border-black p-4"
-      [class]="customClass">
-      <img
-        *ngIf="type === CardType.TEACHER"
-        src="assets/img/teacher.png"
-        width="200px" />
-      <img
-        *ngIf="type === CardType.STUDENT"
-        src="assets/img/student.webp"
-        width="200px" />
-
+      [style.backgroundColor]="backgroundColor">
+      <img [src]="imgSrc" width="200px" />
       <section>
         <app-list-item
           *ngFor="let item of list"
-          [name]="item.firstName"
+          [name]="item[itemName]"
           [id]="item.id"
-          [type]="type"></app-list-item>
+          [store]="store"></app-list-item>
       </section>
-
       <button
         class="rounded-sm border border-blue-500 bg-blue-300 p-2"
         (click)="addNewItem()">
@@ -39,23 +27,15 @@ import { ListItemComponent } from '../list-item/list-item.component';
   standalone: true,
   imports: [NgIf, NgFor, ListItemComponent],
 })
-export class CardComponent {
-  @Input() list: any[] | null = null;
-  @Input() type!: CardType;
-  @Input() customClass = '';
-
-  CardType = CardType;
-
-  constructor(
-    private teacherStore: TeacherStore,
-    private studentStore: StudentStore,
-  ) {}
+export class CardComponent<T extends { id: number; [key: string]: any }> {
+  @Input() list: T[] | null = null;
+  @Input() backgroundColor = '';
+  @Input() store!: Store<T>;
+  @Input() imgSrc!: string;
+  @Input() randomData!: () => T;
+  @Input() itemName = 'name';
 
   addNewItem() {
-    if (this.type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (this.type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
+    this.store.addOne(this.randomData());
   }
 }
