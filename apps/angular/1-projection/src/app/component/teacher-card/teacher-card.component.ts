7a4dd9143ee2e2cae4fb30568f633c7234755bca
Pablo Pastorino <phpastorino@outlook.com>
2024-08-06 11:28:27 -0300

    refactor: student and teacher cards to provide binding to card components

--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,40 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
-
 @Component({
   selector: 'app-teacher-card',
   template: `
     <app-card
       [list]="teachers"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+      itemName="firstName"
+      [store]="store"
+      [randomData]="randTeacher"
+      imgSrc="assets/img/teacher.png"
+      [backgroundColor]="backgroundColor"></app-card>
   `,
-  styles: [
-    `
-      ::ng-deep .bg-light-red {
-        background-color: rgba(250, 0, 0, 0.1);
-      }
-    `,
-  ],
   standalone: true,
   imports: [CardComponent],
 })
 export class TeacherCardComponent implements OnInit {
   teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  randTeacher: () => Teacher = randTeacher;
+  backgroundColor = 'rgba(250, 0, 0, 0.1)';
 
   constructor(
     private http: FakeHttpService,
-    private store: TeacherStore,
+    public store: TeacherStore,
   ) {}
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
-
     this.store.teachers$.subscribe((t) => (this.teachers = t));
   }
 }
